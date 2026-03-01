54cefcd87c039edd569ce6dfd6599c96215759c5
byevsky1978 <byevsky@gmail.com>
2026-03-01 17:27:36 +0200

    final changes to project

--- Project_WOLT/pages/restaurants_page.py
+++ Project_WOLT/pages/restaurants_page.py
@@ -6,7 +6,27 @@ class RestaurantsPage():
     def __init__(self, page):
         self.page = page
 
+    def test_subtitle(self):
+        main_discovery_content = self.page.locator("[data-test-id='MainDiscoveryContent']")
+        subtitle = main_discovery_content.locator("h2").first
+        expect(subtitle).to_have_text("I feel like eating..")
 
-    def most_places_test(self):
+    def test_most_places(self):
         main_discovery_content = self.page.locator("[data-test-id='MainDiscoveryContent']")
         category_list = main_discovery_content.locator("ul").first
+        expect(category_list).to_have_count(1)
+
+        categories = category_list.locator("li")
+        most_places_category = None
+        most_places = 0
+        for category in categories.all():
+            category.scroll_into_view_if_needed()
+            title = category.locator("h3").first
+            subtitle = category.locator("p").first
+            expect(subtitle).to_have_count(1)
+            count = int(subtitle.text_content().split(" ")[0])
+            if count > most_places:
+                most_places = count
+                most_places_category = title.text_content()
+
+        assert most_places_category == 'Kosher'
--- Project_WOLT/tests/main_wolt_test.py
+++ Project_WOLT/tests/main_wolt_test.py
@@ -1,3 +1,5 @@
+from playwright.sync_api import expect
+
 from Project_WOLT.globals import URL
 from Project_WOLT.pages.discovery_page import DiscoveryPage
 from Project_WOLT.pages.restaurants_page import RestaurantsPage
@@ -5,21 +7,30 @@ from Project_WOLT.pages.welcome_page import WelcomePage
 
 
 class TestMain():
-
-
-    def test_main(self,setup_playwright_project_wolt):
+    def test_cookies(self, setup_playwright_project_wolt):
         page = setup_playwright_project_wolt
-        page.goto(URL)
         allow_cookies_button = page.locator("[data-test-id='allow-button']")
+        expect(allow_cookies_button).to_have_count(1)
         allow_cookies_button.click()
 
+    def test_welcome_page(self, setup_playwright_project_wolt):
+        page = setup_playwright_project_wolt
         welcome_page = WelcomePage(page)
         welcome_page.search_for_item("petah tikva")
         welcome_page.test_result_list()
 
+    def test_discovery_page(self, setup_playwright_project_wolt):
+        page = setup_playwright_project_wolt
         discovery_page = DiscoveryPage(page)
         discovery_page.go_to_restaurants()
 
+    def test_restaurants_page_subtitle(self, setup_playwright_project_wolt):
+        page = setup_playwright_project_wolt
         restaurants_page = RestaurantsPage(page)
-        restaurants_page.most_places_test()
+        restaurants_page.test_subtitle()
+
+    def test_restaurants_page_most_places(self, setup_playwright_project_wolt):
+        page = setup_playwright_project_wolt
+        restaurants_page = RestaurantsPage(page)
+        restaurants_page.test_most_places()
 
